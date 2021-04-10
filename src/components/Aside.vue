3f2028af347d934eefcf59d3fd115798c1677e7a
lzb <2082240494@qq.com>
2021-04-10 21:35:32 +0800

    master

---
+++ src/components/Aside.vue
@@ -0,0 +1,100 @@
+<template>
+  <el-menu :default-active="currentmenu.toString()" class="el-menu-vertical-demo" background-color="#545c64"
+    text-color="#fff" active-text-color="#ffd04b">
+    <h3 class="sysname">快餐店管理系统</h3>
+    <el-menu-item :index="index.toString()" v-for="(item, index) in nsMenu" :key="index" @click="changemenu(index)">
+      <i :class="'el-icon-' + item.icon"></i>
+      <span slot="title">
+        {{ item.label }}
+      </span>
+    </el-menu-item>
+    <el-submenu :index="(index + nsMenu.length).toString()" v-for="(item, index) in hsMenu"
+      :key="index + nsMenu.length">
+      <template slot="title">
+        <i class="el-icon-location"></i>
+        <span>{{ item.label }}</span>
+      </template>
+      <el-menu-item-group>
+        <el-menu-item :index="(index + nsMenu.length).toString()" v-for="(sonitem, index) in item.children"
+          :key="index + nsMenu.length">
+          <i :class="'el-icon-' + sonitem.icon"></i>
+          <router-link :to="sonitem.path" class="router">{{ sonitem.label }}
+          </router-link>
+        </el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+  </el-menu>
+</template>
+
+<script>
+  import { mapState } from "vuex";
+  export default {
+    computed: {
+      ...mapState({
+        menu: state => state.menu.menu,
+        currentmenu: state => state.menu.currentmenu,
+      }),
+      nsMenu() {
+        return this.menu.filter((item) => !item.children);
+      },
+      hsMenu() {
+        return this.menu.filter((item) => item.children);
+      },
+    },
+    watch: {
+      currentmenu() {
+        switch (this.$store.state.menu.currentmenu) {
+          case 0: return this.$router.push('/').catch(err => {
+            console.log('不能重复点击导航，有点问题啊',err)
+          })
+          case 1: return this.$router.push('/shop').catch(err => {
+            console.log('不能重复点击导航，有点问题啊',err)
+          })
+          case 2: return this.$router.push('/user').catch(err => {
+            console.log('不能重复点击导航，有点问题啊',err)
+          })
+        }
+      }
+    },
+    methods: {
+      changemenu(index) {
+        this.$store.commit("selectMenu", index);
+      },
+    },
+    mounted() {
+      if(this.$store.state.menu.currentmenu==0){this.$router.push('/')}
+    },
+  };
+</script>
+
+<style scoped lang="less">
+  .el-menu {
+    .sysname {
+      color: rgba(154, 225, 240, 0.87);
+      text-align: center;
+    }
+
+    height: 100vh;
+
+    &-item {
+      padding: 0;
+      margin: 0;
+    }
+
+    &-vertical-demo.el-menu {
+      width: 12vw;
+      min-width: 150px;
+      border: 0;
+
+      .router {
+        text-decoration: none;
+        color: inherit;
+      }
+    }
+  }
+
+  .el-submenu .el-menu-item {
+    width: 12%;
+    min-width: 150px;
+  }
+</style>
---
+++ src/views/user/user.vue
@@ -0,0 +1,71 @@
+<template>
+    <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="用户名称">
+            <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="常住地址">
+            <el-select v-model="form.region" placeholder="请选择常住地址">
+                <el-option label="区域一" value="shanghai"></el-option>
+                <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+        </el-form-item>
+        <el-form-item label="生日">
+            <el-col :span="11">
+                <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;">
+                </el-date-picker>
+            </el-col>
+        </el-form-item>
+        <el-form-item label="信息可见">
+            <el-switch v-model="form.delivery"></el-switch>
+        </el-form-item>
+        <el-form-item label="用户喜好">
+            <el-checkbox-group v-model="form.type">
+                <el-checkbox label="美食/网红零食" name="type"></el-checkbox>
+                <el-checkbox label="优质穿搭" name="type"></el-checkbox>
+                <el-checkbox label="可爱玩偶" name="type"></el-checkbox>
+            </el-checkbox-group>
+        </el-form-item>
+        <el-form-item label="特殊资源">
+            <el-radio-group v-model="form.resource">
+                <el-radio label="球鞋/手账/盲盒"></el-radio>
+                <el-radio label="线下剧本杀/密室逃脱"></el-radio>
+            </el-radio-group>
+        </el-form-item>
+        <el-form-item label="活动形式">
+            <el-input type="textarea" v-model="form.desc"></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary" @click="onSubmit">立即创建</el-button>
+            <el-button>取消</el-button>
+        </el-form-item>
+    </el-form>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                form: {
+                    name: '',
+                    region: '',
+                    date1: '',
+                    delivery: false,
+                    type: [],
+                    resource: '',
+                    desc: ''
+                }
+            }
+        },
+        methods: {
+            onSubmit() {
+                console.log('submit!');
+            }
+        }
+    }
+</script>
+
+<style scoped lang="less">
+    .el-form{
+        margin: 2%;
+    }
+</style>
